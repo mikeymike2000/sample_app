4c83d7a5c47b3e3a33e33fae821c7c0c514f9778
Mike Arnold <dharmamike@gmail.com>
2011-09-09 21:07:27 -0500

    completed ch 3 exercises

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ end
 
 group :test do
 	gem 'webrat', '0.7.1'
+	gem 'autotest', '4.4.6'
+    gem 'autotest-rails-pure', '4.1.2'
+    gem 'autotest-fsevent', '0.2.4'
+    gem 'autotest-growl', '0.2.9'
 end
 
 # Use unicorn as the web server
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+  	@base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,9 +15,9 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-			response.should have_selector("title", 
-																		:content => "Ruby on Rails Tutorial Sample App | Home")
-		end
+	  response.should have_selector("title", 
+	                                :content => @base_title + " | Home")
+	end
   end
 
   describe "GET 'contact'" do
@@ -24,9 +28,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-			response.should have_selector("title", 
-																		:content => "Ruby on Rails Tutorial Sample App | Contact")
-		end
+	  response.should have_selector("title", :content =>  @base_title + " | Contact")
+	end
   end
 
   describe "GET 'about'" do
@@ -36,10 +39,21 @@ describe PagesController do
     end
 
     it "should have the right title" do
-      get 'about'
-			response.should have_selector("title", 
-																		:content => "Ruby on Rails Tutorial Sample App | About")
-		end
+      get 'about' 
+      response.should have_selector("title", :content =>  @base_title + " | About")
+	end
   end
-
+  
+  describe "GET 'help'" do
+  	it "should be successful" do
+  	  get 'help'
+  	  response.should be_success
+  	end
+  	
+  	it "should have the right title" do
+  	  get 'help'
+  	  response.should have_selector("title", :content =>  @base_title + " | Help")
+  	end
+  end
+  
 end
